a45fc7212ffee5cce233b18385fa8eb29b94aa2e
pmtisoy <pmtisoy@gmail.com>
2024-11-30 20:26:42 +0800

    game1

--- GameApp/urls.py
+++ GameApp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('welcome/', views.welcome_view, name='welcome'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('index/',views.game1_view,name='game1'),
+    path('index/', views.game1_view, name='game1'),
 ]
